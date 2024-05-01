875e9591a015926e5164cafb20cff68453558b51
Nilmir <vasilenko.vlad.lycey@gmail.com>
2024-05-01 13:32:39 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsLab4/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsLab4
+{
+    class Fraction
+    {
+        private int lerror;
+        private int a;
+        private int b;
+        private double answ;
+
+        public Fraction(int a, int b)
+        {
+            if (b != 0)
+            {
+                lerror = 0;
+                this.a = a;
+                this.b = b;
+                this.answ = a / b;
+            }
+            else { lerror = 1; }
+        }
+
+        public int GetError()
+        {
+            return lerror;
+        }
+
+        public double GetAnsw()
+        {
+            return answ;
+        }
+
+        public Fraction Reverse()
+        {
+            Fraction reversed = new Fraction(this.b, this.a);
+
+            return reversed;
+        }
+
+        public string toString()
+        {
+            string returner="";
+
+            returner = this.a + "/" + this.b;
+
+            return returner;
+        }
+
+        public static Fraction operator +(Fraction left, Fraction right)
+        {
+
+            Fraction Summ =new Fraction(left.a*right.b+right.a*left.b,left.b*right.b);
+            if (Summ.b == 0)
+            { 
+                Summ.lerror = 1; 
+            }
+            else
+            {
+                Summ.lerror = 0;
+            }
+                return Summ;
+        }
+
+        public static Fraction operator -(Fraction left, Fraction right)
+        {
+
+            Fraction Summ = new Fraction(left.a * right.b - right.a * left.b, left.b * right.b);
+            if (Summ.b == 0)
+            {
+                Summ.lerror = 1;
+            }
+            else
+            {
+                Summ.lerror = 0;
+            }
+            return Summ;
+        }
+
+        public static Fraction operator *(Fraction left, Fraction right)
+        {
+            Fraction Summ = new Fraction(left.a * right.a, left.b * right.b);
+            if (Summ.b == 0)
+            {
+                Summ.lerror = 1;
+            }
+            else
+            {
+                Summ.lerror = 0;
+            }
+            return Summ;
+        }
+    }
+}
---
+++ WindowsFormsLab4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fraction frac1 = new Fraction(2, 3);
+
+            if (frac1.GetError() == 0)
+            {
+                Fraction frac2 = frac1.Reverse();
+                if (frac2.GetError() == 0)
+                {
+                    string strfrac1 = frac1.toString();
+                    string strfrac2 = frac2.toString();
+                    textBox1.Text = "Fraction1 = "+strfrac1+Environment.NewLine+"reverse Fraction1 = " + strfrac2;
+                }
+                else { textBox1.Text = "error, fraction with a numerator equal to 0 can't be reversed."; }
+            }
+            else { textBox1.Text = "error, denominator can't be equal to 0."; }
+            int i = 1 + 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Fraction f1 = new Fraction(2, 3);
+            Fraction f2 = new Fraction(4, 5);
+            Fraction f3 = new Fraction(6, 7);
+
+            string init = "f1 = " + f1.toString() + Environment.NewLine + "f2 = " + f2.toString() + Environment.NewLine + "f3 = " + f3.toString() + Environment.NewLine;
+
+            Fraction R = (f1 + f2) * (f1 - f3);
+
+            textBox2.Text =init + Environment.NewLine + "R = " + R.toString() + Environment.NewLine + "1/R = " + R.Reverse().toString();
+        }
+    }
+}
